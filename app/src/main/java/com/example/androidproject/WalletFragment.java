8a4036e35419a55b249c59ef4ad01a2ca541e5c0
Lotfiayoub1 <63500139+Lotfiayoub1@users.noreply.github.com>
2021-04-14 13:25:31 +0100

    Implement Wallet System

--- app/src/main/java/com/example/androidproject/WalletFragment.java
+++ app/src/main/java/com/example/androidproject/WalletFragment.java
@@ -7,11 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.androidproject.model.CategoryModel;
 import com.example.androidproject.model.User;
+import com.example.androidproject.model.WithdrawRequest;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -23,6 +27,9 @@ public class WalletFragment extends Fragment {
 
     FirebaseFirestore database;
     TextView currentCoins;
+    User user;
+    Button sendRequest;
+    EditText emailBox;
 
     public WalletFragment() {
         // Required empty public constructor
@@ -47,10 +54,35 @@ public class WalletFragment extends Fragment {
                 .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
-                User user = documentSnapshot.toObject(User.class);
+                user = documentSnapshot.toObject(User.class);
                 currentCoins.setText(user.getCoins() + "");
             }
         });
+
+        emailBox = view.findViewById(R.id.emailBox1);
+        sendRequest = view.findViewById(R.id.sendRequest);
+        sendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.getCoins() > 50000){
+                    String uid = FirebaseAuth.getInstance().getUid();
+                    String email = emailBox.getText().toString();
+                    WithdrawRequest withdrawRequest = new WithdrawRequest(uid,email,user.getName());
+                    database.collection("withdraws")
+                            .document(uid)
+                            .set(withdrawRequest).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(getContext(), "Request Sent successfully.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }else{
+                    Toast.makeText(getContext(), "You need more coins to get withdraw ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        
+        
 
         return view;
     }
